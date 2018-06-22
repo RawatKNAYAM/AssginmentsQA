97ae351369ca39a9fd5943aefb140ea6111224e9
RawatKNAYAM <40259395+RawatKNAYAM@users.noreply.github.com>
2018-06-22 19:08:01 +0530

    Update testPopup.java

--- testPopup.java
+++ testPopup.java
@@ -4,16 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
-import com.gargoylesoftware.htmlunit.javascript.host.Set;
-
 class testPopup {
 	public static void main(String[] args)
 	{
 	WebDriver driver = new ChromeDriver();
 	driver.get("http://10.0.1.86//tatoc");
 	
-	//main window for popup retrival
 	String mainWindow = driver.getWindowHandle();	
 		
 	driver.findElement(By.linkText("Basic Course")).click();
@@ -35,8 +31,7 @@ class testPopup {
 	}
 	}
 	
-	//handling drag and drop
-	
+		
 	WebElement element = driver.findElement(By.id("dragbox")); 
 
 	WebElement target = driver.findElement(By.id("dropbox"));
@@ -57,33 +52,20 @@ class testPopup {
     
     driver.switchTo().window(mainWindow);
     driver.findElements(By.cssSelector("a")).get(1).click();
-
-	//driver.findElement(By.linkText("Proceed")).click();
-	
-    //generating token
     driver.findElement(By.cssSelector("a")).click();
-    
-    //String value = driver.findElement(By.id("token")).getText().split("Token: ")[1];    
-    
-    
     String Tokenvalue = driver.findElement(By.id("token")).getText();
      Tokenvalue = (Tokenvalue.substring(7));
+     System.out.println(Tokenvalue);
     
-    System.out.println(Tokenvalue);
     
-    //adding cookie
     Cookie ck = new Cookie("Token", Tokenvalue);
     driver.manage().addCookie(ck);
     
 	
     
-    //driver.manage().addCookie( new Cookie("Token", Tokenvalue, "/")); 
+     
     driver.findElements(By.cssSelector("a")).get(1).click(); 
 	
 	}
 
-	private static WebDriver getMyDriver() {
-		// TODO Auto-generated method stub
-		return null;
 	}
-}
